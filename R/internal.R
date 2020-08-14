b4dc52fd39a1e3fc214ae377e117956b4d8b4521
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-08-15 00:11:10 +0100

    update internal functions

--- R/internal.R
+++ R/internal.R
@@ -1,12 +1,4 @@
 
-# nocov start
+check_true_or_false <- function(x){
 
-release_questions <- function(){
-
-  c(
-    "Have you updated the version number in inst/CITATION (two fields)?",
-    "Have you run all the tests listed on the cran-comments.md?"
-  )
 }
-
-# nocov end
