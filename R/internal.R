7634eb746b557f183c4e4a0c4969ec2831f4574b
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-08-14 23:05:56 +0100

    initialize R

---
+++ R/internal.R
@@ -0,0 +1,12 @@
+
+# nocov start
+
+release_questions <- function(){
+
+  c(
+    "Have you updated the version number in inst/CITATION (two fields)?",
+    "Have you run all the tests listed on the cran-comments.md?"
+  )
+}
+
+# nocov end
