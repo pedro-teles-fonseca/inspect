76ac706b1b8cec63971f8dbf676dc2102124f522
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-08-17 02:31:14 +0100

    Improve documentation

--- R/inspect_.R
+++ R/inspect_.R
@@ -4,7 +4,7 @@
 #' @description `inspect_prob` checks if an object is a numeric vector of valid probability values. This can be useful to validate inputs, intermediate calculations or outputs in user-defined functions.
 #'
 #' @param x An arbitrary object.
-#' @param allow_nas Logical value indicating either `TRUE` or `FALSE`. If `FALSE` then the execution is stopped (via \code{\link[base]{stop}}) in case there are `NA` or `NaN` values in `x`.
+#' @param allow_nas Logical value. If `TRUE` then `NA` and `NaN` values in `x` are allowed (and generate a warning message). If `FALSE`, execution is stopped and an error message is thrown in case there are `NA` or `NaN` values in `x`.
 #'
 #' @details `inspect_prob` conducts a series of tests to check if `x` is a numeric vector of valid probability values. Namely, `inspect_prob` checks if:
 #' * `x` is `NULL` or empty.
@@ -32,17 +32,17 @@
 #' inspect_prob(seq(0, 1, 0.1))
 #'
 #' # Calls that throw an informative warning message:
-#' y <- c(0.1, 0.2, NA, 0.4, 0.5)
+#' \dontrun{y <- c(0.1, 0.2, NA, 0.4, 0.5)}
 #' \dontrun{inspect_prob(y)}
 #' \dontrun{inspect_prob(y, allow_nas = TRUE)}
 #'
 #' # Calls that throw an informative error message:
-#' z1 <- c(-0.9, 0, 0.1, 0.2, 0.3, 0.4, 0.5)
+#' \dontrun{z1 <- c(-0.9, 0, 0.1, 0.2, 0.3, 0.4, 0.5)}
 #' \dontrun{inspect_prob(z1)}
-#' z2 <- c(-0.9, 0, 0.1, 0.2, 0.3, 0.4, 0.5)
+#' \dontrun{z2 <- c(-0.9, 0, 0.1, 0.2, 0.3, 0.4, 0.5)}
 #' \dontrun{inspect_prob(z2, allow_nas = FALSE)}
-#' mylist <- list(NULL, TRUE, factor(.5), matrix(0.5),
-#'          "0.5", list(0.5), NA, NaN, 1.1, .5)
+#' \dontrun{mylist <- list(NULL, TRUE, factor(.5), matrix(0.5),
+#'          "0.5", list(0.5), NA, NaN, 1.1, -0.5)}
 #' \dontrun{inspect_prob(mylist)}
 #' \dontrun{inspect_prob(mylist[[1]])}
 #' \dontrun{inspect_prob(mylist[[2]])}
@@ -117,14 +117,15 @@ inspect_prob <- function(x, allow_nas = TRUE){
 #' inspect_bf(seq(10, 100, 10))
 #'
 #' # Call that throws an informative warning message:
-#' y <- c(0.1, 0.2, NA, 0.4, 0.5)
+#' \dontrun{y <- c(0.1, 0.2, NA, 0.4, 0.5)}
 #' \dontrun{inspect_bf(y)}
 #'
 #' # Calls that throw informative error messages:
-#' z <- c(-0.9, 0, 0.1, 0.2, 0.3, 0.4, 0.5)
+#' \dontrun{z <- c(-0.9, 0, 0.1, 0.2, 0.3, 0.4, 0.5)}
 #' \dontrun{inspect_bf(z)}
-#' mylist <- list(NULL, TRUE, factor(.5), matrix(0.5),
-#'          "0.5", list(0.5), NA, NaN, -0.5, -5)
+#' \dontrun{mylist <- list(NULL, TRUE, factor(.5), matrix(0.5),
+#'          "0.5", list(0.5), NA, NaN, -0.5, -5)}
+#' \dontrun{inspect_bf(mylist)}
 #' \dontrun{inspect_bf(mylist[[1]])}
 #' \dontrun{inspect_bf(mylist[[2]])}
 #' \dontrun{inspect_bf(mylist[[3]])}
@@ -192,14 +193,15 @@ inspect_bf <- function(x){
 #' inspect_log_bf(y)
 #'
 #' # Call that throws an informative warning message:
-#' z <- c(0.1, 2, NA, 40, 0.5)
+#' \dontrun{z <- c(0.1, 2, NA, 40, 0.5)}
 #' \dontrun{inspect_log_bf(z)}
 #'
 #' # Calls that throw informative error messages:
-#' z <- c(-0.9, 0, 0.1, 0.2, 0.3, 0.4, 0.5)
+#' \dontrun{z <- c(-0.9, 0, 0.1, 0.2, 0.3, 0.4, 0.5)}
 #' \dontrun{inspect_bf(z)}
-#' mylist <- list(NULL, TRUE, factor(.5), matrix(0.5),
-#'          "0.5", list(0.5), NA, NaN)
+#' \dontrun{mylist <- list(NULL, TRUE, factor(.5), matrix(0.5),
+#'          "0.5", list(0.5), NA, NaN)}
+#' \dontrun{inspect_log_bf(mylist)}
 #' \dontrun{inspect_log_bf(mylist[[1]])}
 #' \dontrun{inspect_log_bf(mylist[[2]])}
 #' \dontrun{inspect_log_bf(mylist[[3]])}
@@ -261,8 +263,9 @@ inspect_log_bf <- function(x){
 #' inspect_log_base(0.5)
 #'
 #' # Calls that throw informative error messages:
-#' mylist <- list(NULL, numeric(0), -1, 0, TRUE, 10,
-#'                factor(10), list(10), matrix(10), NaN, NA)
+#' \dontrun{mylist <- list(NULL, numeric(0), TRUE, factor(10),
+#'         list(10), matrix(10), NaN, NA, -1, 0)}
+#' \dontrun{inspect_log_base(mylist)}
 #' \dontrun{inspect_log_base(mylist[[1]])}
 #' \dontrun{inspect_log_base(mylist[[2]])}
 #' \dontrun{inspect_log_base(mylist[[3]])}
@@ -273,7 +276,6 @@ inspect_log_bf <- function(x){
 #' \dontrun{inspect_log_base(mylist[[8]])}
 #' \dontrun{inspect_log_base(mylist[[9]])}
 #' \dontrun{inspect_log_base(mylist[[10]])}
-#' \dontrun{inspect_log_base(mylist[[11]])}
 #'
 #' @export
 
@@ -300,11 +302,11 @@ inspect_log_base <- function(x){
 #'
 #' @description `inspect_scale` checks if an object is a string of characters representing one of the Bayes factor interpretation scales available in the `pcal` package. This can be useful to validate inputs in user-defined functions.
 #'
-#' @param scale An arbitrary object.
+#' @param x An arbitrary object.
 #'
 #' @details `inspect_scale` conducts a series of tests to check if `scale` is a string of characters representing one of the Bayes factor interpretation scales available in the `pcal` package. Namely, `inspect_scale` checks if:
 #' * `scale` is `NULL` or empty.
-#' * `scale` is a character, atomic vector of \code{\link[base]{length}} 1 specifying either "Jeffreys" or "Kass-Raftery" (not case sensitive).
+#' * `scale` is an atomic vector of type character and \code{\link[base]{length}} 1 specifying either "Jeffreys" or "Kass-Raftery" (not case sensitive).
 #' * `scale` is `NA` or `NaN`.
 #'
 #' @return `inspect_scale` does not return any output. There are two possible scenarios:
@@ -325,32 +327,36 @@ inspect_log_base <- function(x){
 #' inspect_scale("Kass-Raftery")
 #'
 #' # Calls that throw informative error messages:
-#' \dontrun{inspect_scale(NULL)}
-#' \dontrun{inspect_scale(10)}
-#' \dontrun{inspect_scale("Bayes")}
-#' \dontrun{inspect_scale("jeff")}
-#' \dontrun{inspect_scale("kassraftery")}
-#' \dontrun{inspect_scale(c("jeffreys", "kass-raftery"))}
+#' \dontrun{mylist <- list(NULL, NA, 10, "Bayes", "Jeff",
+#'           "kassraftery", c("jeffreys", "kass-raftery"))}
+#' \dontrun{inspect_scale(mylist)}
+#' \dontrun{inspect_scale(mylist[[1]])}
+#' \dontrun{inspect_scale(mylist[[2]])}
+#' \dontrun{inspect_scale(mylist[[3]])}
+#' \dontrun{inspect_scale(mylist[[4]])}
+#' \dontrun{inspect_scale(mylist[[5]])}
+#' \dontrun{inspect_scale(mylist[[6]])}
+#' \dontrun{inspect_scale(mylist[[7]])}
 #'
 #' @export
 
-inspect_scale <- function(scale){
+inspect_scale <- function(x){
 
-  output_name <- paste0("'", deparse(substitute(scale)), "'")
+  output_name <- paste0("'", deparse(substitute(x)), "'")
 
-  if(is.null(scale)){
+  if(is.null(x)){
     stop(paste("Invalid argument:", output_name, "is NULL."))
   }
-  if(any(!is.vector(scale), !is.atomic(scale), isFALSE(length(scale) == 1))){
+  if(any(!is.vector(x), !is.atomic(x), isFALSE(length(x) == 1))){
     stop(paste("Invalid argument:", output_name, "must be an atomic vector of length 1."))
   }
-  if(is.na(scale)){
+  if(is.na(x)){
     stop(paste("Invalid argument:", output_name, "is NA or NaN."))
   }
-  if(!is.character(scale)){
+  if(!is.character(x)){
     stop(paste("Invalid argument: the type of", output_name, "must be character."))
   }
-  if(isFALSE(tolower(scale) %in% c("jeffreys", "kass-raftery"))){
+  if(isFALSE(tolower(x) %in% c("jeffreys", "kass-raftery"))){
     stop(paste("Invalid argument:", output_name, "must be either 'jeffreys' or 'kass-raftery'."))
   }
 }
@@ -358,6 +364,17 @@ inspect_scale <- function(scale){
 inspect_true_or_false <- function(x){
 
   output_name <- paste0("'", deparse(substitute(x)), "'")
+
+  if(is.null(x)){
+    stop(paste("Invalid argument:", output_name, "is NULL."))
+  }
+
+  if(any(!is.vector(x), !is.atomic(x), !is.logical(x), isTRUE(length(x) == 1))){
+    stop(paste("Invalid argument:", output_name, "must be an logical vector of length 1."))
+  }
+  if(is.na(x)){
+    stop(paste("Invalid argument:", output_name, "is NA or NaN."))
+  }
 
 }
 
