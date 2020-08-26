3cb6686987fcaa2c3eb0ced3e034a2ac7b7a1826
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-08-26 17:04:01 +0100

    update release questions function

--- R/internal.R
+++ R/internal.R
@@ -7,7 +7,8 @@ release_questions <- function(){
 
   c(
     "Have you updated the version number in inst/CITATION (two fields)?",
-    "Have you run all the tests listed on the cran-comments.md?"
+    "Have you run all the tests listed in cran-comments.md?",
+    "Have you evaluated the impact in downstream dependencies?"
   )
 }
 
