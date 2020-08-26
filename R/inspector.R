f67aa08ab924d42f0233e9d73af8f68356b67dec
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-08-26 18:29:34 +0100

    document package

---
+++ R/inspector.R
@@ -0,0 +1,15 @@
+
+#' @importFrom Rdpack reprompt
+
+#' @title Inspector Package
+#'
+#' @description Validation of arguments and objects in user-defined functions.
+#'
+#' @details The `inspector` package provides utility functions that implement and automate common sets of validation tasks. These functions are particularly useful to validate inputs, intermediate objects and output values in user-defined functions, resulting in tidier and less verbose functions.
+#'
+#' For a complete list of functions see `library(help = "inspector")`.
+#'
+#' @docType package
+#' @name inspector
+
+"_PACKAGE"
