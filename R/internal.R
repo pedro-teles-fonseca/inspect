b4ab8bd1ca62c870fff4bbe8c4ffb44ff3f3a220
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-08-28 04:53:46 +0100

    Update internal.R

--- R/internal.R
+++ R/internal.R
@@ -10,7 +10,7 @@ release_questions <- function(){
   c(
     "Have you updated the version number in inst/CITATION (two fields)?",
     "Have you run all the tests listed in cran-comments.md?",
-    "Have you evaluated the impact in downstream dependencies?"
+    "Have you evaluated the impact on downstream dependencies?"
   )
 }
 
